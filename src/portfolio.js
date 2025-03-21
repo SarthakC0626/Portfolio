d2c0715c25fd1bf97956e588def2e44ab83c87b8
Sarthak <122520596+SarthakC0626@users.noreply.github.com>
2025-03-21 10:33:51 +0530

    Update portfolio.js
    lets see

--- src/portfolio.js
+++ src/portfolio.js
@@ -36,6 +36,10 @@ const socialMediaLinks = {
   github: "https://github.com/Lucifer0906",
   linkedin: "https://www.linkedin.com/in/sarthak-chauhan-a43069246/",
   gmail: "sarthakchauhan0911@gmail.com",
+  gitlab: "https://gitlab.com/saadpasta",
+  facebook: "https://www.facebook.com/saad.pasta7",
+  medium: "https://medium.com/@saadpasta",
+  stackoverflow: "https://stackoverflow.com/users/10422806/saad-pasta",
     // Instagram, Twitter and Kaggle are also supported in the links!
   // To customize icons and social links, tweak src/components/SocialMedia
   display: true // Set true to display this section, defaults to false
